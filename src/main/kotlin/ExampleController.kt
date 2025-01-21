d71c62d4f5e85da83e7742228f93ec2dd813c2e4
andrew.pirkl <andrew.pirkl@jamf.com>
2025-01-21 12:52:11 -0600

    create data binding example

---
+++ src/main/kotlin/ExampleController.kt
@@ -0,0 +1,15 @@
+package com.jamfschool
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+    @GetMapping("/test")
+    fun index(filterExample: FilterExample): String {
+        return "Greetings from Spring Boot!"
+    }
+}
+
+data class FilterExample(@RequestParam(required = false) var testMap: Map<String,String>? = null)
---
+++ src/test/kotlin/ExampleControllerTest.kt
@@ -0,0 +1,24 @@
+import com.jamfschool.Application
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.request
+
+@AutoConfigureMockMvc
+@SpringBootTest(classes = [Application::class])
+class ExampleControllerTest {
+     @Autowired
+     private lateinit var mockMvc: MockMvc
+
+     @Test
+     fun `should return default message`() {
+         mockMvc.get("/test?testMap[operator]=<&testMap[value]=12345") {
+         }.andExpect {
+             status { isOk() }
+             content { string("Greetings from Spring Boot!") }
+         }
+     }
+}
